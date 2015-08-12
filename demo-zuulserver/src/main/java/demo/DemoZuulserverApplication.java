c93cfacc5185624875462dc26f8534136f76b5e8
karimgarza <karimgarza@gmail.com>
2015-08-12 11:04:12 -0500

    .

--- demo-zuulserver/src/main/java/demo/DemoZuulserverApplication.java
+++ demo-zuulserver/src/main/java/demo/DemoZuulserverApplication.java
@@ -1,14 +1,36 @@
 package demo;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.AbstractProtocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 
 @SpringBootApplication
 @EnableZuulProxy
-public class DemoZuulserverApplication {
+public class DemoZuulserverApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoZuulserverApplication.class, args);
     }
+    
+    @SuppressWarnings("unused")
+    private static final Logger LOG = LoggerFactory.getLogger(DemoZuulserverApplication.class);
+
+    @Bean
+    public EmbeddedServletContainerCustomizer embeddedServletCustomizer(){
+        return new MyEmbeddedServletContainerCustomizer();
+    }
+   
 }
